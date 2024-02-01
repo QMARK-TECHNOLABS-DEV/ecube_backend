bef3731364331d6f3bbc46eff81ffa160425d878
Noel6161131110 <noelpaulgeorge@gmail.com>
2024-02-01 17:33:16 +0530

    changes in serializer

--- register_student/serializers.py
+++ register_student/serializers.py
@@ -5,14 +5,14 @@ class StudentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Student
-        fields = ['id','name', 'admission_no','phone_no', 'school_name','class_name', 'division','class_group','batch_year', 'subjects', 'email_id']      
+        fields = ['id', 'name', 'admission_no','phone_no', 'school_name','class_name', 'division','class_group','batch_year', 'subjects', 'email_id']      
 
 
 class StudentDisplaySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Student
-        fields = ['name', 'admission_no','phone_no', 'school_name', 'subjects', 'email_id'] 
+        fields = ['id', 'name', 'admission_no','phone_no', 'school_name', 'subjects', 'email_id'] 
         
 class ExamStudentDisplaySerializer(serializers.ModelSerializer):
         class Meta:
